ac01f048567863e3e8ccbfbf213e5a25f9745568
adroullier <arndt@nive.co>
2015-08-02 15:52:08 +0200

    filehost client

--- pynive_client/filehost.py
+++ pynive_client/filehost.py
@@ -1,1 +1,102 @@
-__author__ = 'ad'
+# (c) 2013-2014 Nive GmbH - nive.io
+# This file is released under the BSD-License.
+#
+# Nive User service python client
+# ------------------------------------------------
+# Documentation: http:#www.nive.co/docs/webapi/useraccounts.html#api
+#
+"""
+**Example code 1**
+
+
+::
+
+    from pynive_client import filehost
+
+    storage = filehost.Files(name='mystorage',domain='mydomain')
+
+    # list items
+    result = storage.list(sort='path', order='<', size=20, start=1)
+    for item in result:
+        print item["name"], item["size"]
+
+
+
+**Example code 2**
+
+Retrieve a security token and add, update, get and remove a item
+
+::
+
+    from pynive_client import user
+    from pynive_client import filehost
+
+    niveuser = user.User(domain='mydomain')
+
+    # retrieve a token to connect to the data storage service
+    token = niveuser.token(identity='username', password='userpw')
+
+    storage = filehost.Files(name='mystorage',domain='mydomain',token=token)
+
+    # get a files infos
+    file = storage.getItem({"path": "/index.html"})
+    print file.name, file.mime, file.size
+
+"""
+
+import endpoint
+
+
+class Files(endpoint.Client):
+
+    default_version='api'
+
+    def __init__(self, name, domain=None, session=None, **options):
+        """
+
+        :param name: data storage instance name
+        :param domain: domain the service is part of
+        :param session: http session object
+        :param options: other endpoint options. see endpoint.py.
+        """
+        super(Files, self).__init__(name=name,
+                                    domain=domain,
+                                    session=session,
+                                    **options)
+        if not "version" in self.options:
+            self.options["version"] = self.default_version
+
+
+    def getItem(self, path, **reqSettings):
+        """
+
+        :param path:
+        :param reqSettings:
+        :return: file wrapper: name, size, type, mime, mtime
+        """
+        values = dict(path=path)
+        content, response = self.call('getItem', values, reqSettings)
+        # todo file wrapper
+        return content
+
+
+    def list(self, path=None, filetype=None, **reqSettings):
+        """
+
+        :param path:
+        :param filetype:
+        :param reqSettings:
+        :return: item result set {"items":[files]}
+        """
+        values = {}
+        if path is not None:
+            values["path"] = path
+        if filetype is not None:
+            values["type"] = filetype
+        content, response = self.call('list', values, reqSettings)
+        # todo result set class with iterator
+        if not content:
+            return {"items": ()}
+        return content
+
+
---
+++ pynive_client/tests/test_filehost.py
@@ -0,0 +1,309 @@
+
+import unittest
+import json
+import logging
+
+from pynive_client import adapter
+from pynive_client import endpoint
+from pynive_client import filehost
+
+
+class filehostTest(unittest.TestCase):
+
+    def setUp(self):
+        logging.basicConfig()
+
+    def test_setup_empty(self):
+        storage = filehost.Files(name="mystorage")
+        self.assertFalse(storage.options["domain"])
+        self.assertFalse(storage.session)
+        self.assert_(storage.options["name"]=="mystorage")
+        self.assert_(storage.options["version"]==storage.default_version)
+
+    def test_setup(self):
+        storage = filehost.Files(name="mystorage",domain="mydomain")
+        self.assert_(storage.options["domain"]=="mydomain")
+        self.assert_(storage.options["name"]=="mystorage")
+        self.assert_(storage.options["version"]==storage.default_version)
+        self.assertFalse(storage.session)
+
+        storage = filehost.Files(name="mystorage", domain="mydomain", version="api23")
+        self.assert_(storage.options["domain"]=="mydomain")
+        self.assert_(storage.options["name"]=="mystorage")
+        self.assert_(storage.options["version"]=="api23")
+        self.assertFalse(storage.session)
+
+    def test_setup_fails(self):
+        storage = filehost.Files(name=None,domain="mydomain")
+        self.assertRaises(endpoint.EndpointException, storage.call, "test", {}, {})
+
+    def test_session(self):
+        storage = filehost.Files(name="mystorage", domain="mydomain", session=adapter.MockAdapter())
+        self.assert_(storage.options["domain"]=="mydomain")
+        self.assert_(storage.options["name"]=="mystorage")
+        self.assert_(storage.session)
+
+
+class filehostFunctionTestKeyMode(unittest.TestCase):
+
+    def setUp(self):
+        logging.basicConfig()
+        session = adapter.MockAdapter()
+        self.storage = filehost.Files(name="mystorage", domain="mydomain", session=session)
+
+    def test_getItem(self):
+        # getItem
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {"name":"index.html", "type":"f", "size":2312,
+                                                  "mime":"text/html", "mtime":"2014/03/01 12:15:10"},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        item = self.storage.getItem(path="/index.html")
+        self.assertEqual(item["name"], "index.html")
+        self.assertEqual(item["type"], "f")
+        self.assertEqual(item["size"], 2312)
+        self.assertEqual(item["mime"], "text/html")
+        self.assertEqual(item["mtime"], "2014/03/01 12:15:10")
+
+    def test_getItem_failure(self):
+        # getItem not found
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        item = self.storage.getItem(path="image.jpg")
+        self.assertFalse(item)
+
+        # empty path
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 400,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.InvalidParameter, self.storage.getItem, path="")
+
+    def test_getItem_codes(self):
+        # code 400
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 400,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.InvalidParameter, self.storage.getItem, path="")
+
+        # code 403
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 403,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.Forbidden, self.storage.getItem, path="test")
+
+        # code 404
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 404,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.ClientFailure, self.storage.getItem, path="test")
+
+        # code 500
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="getItem",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 500,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.ServiceFailure, self.storage.getItem, path="test")
+
+
+    def test_list(self):
+        # list
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {"items":[
+                                          {"name":"index.html", "type":"f", "size":2312,
+                                           "mime":"text/html", "mtime":"2014/03/01 12:15:10"},
+                                          {"name":"image.jpg", "type":"f", "size":10234,
+                                           "mime":"image/jpg", "mtime":"2014/03/01 14:18:21"},
+                                          {"name":"subfolder", "type":"d", "size":0,
+                                           "mime":"", "mtime":"2014/03/03 12:00:00"}
+                                      ]},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        result = self.storage.list()
+        self.assert_(len(result["items"])==3)
+        self.assertEqual(result["items"][0]["name"], "index.html")
+
+        # list root
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {"items":[
+                                          {"name":"index.html", "type":"f", "size":2312,
+                                           "mime":"text/html", "mtime":"2014/03/01 12:15:10"},
+                                          {"name":"image.jpg", "type":"f", "size":10234,
+                                           "mime":"image/jpg", "mtime":"2014/03/01 14:18:21"},
+                                          {"name":"subfolder", "type":"d", "size":0,
+                                           "mime":"", "mtime":"2014/03/03 12:00:00"}
+                                      ]},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        result = self.storage.list(path="/")
+        self.assert_(len(result["items"])==3)
+        self.assertEqual(result["items"][0]["name"], "index.html")
+
+        # list folder
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {"items":[
+                                          {"name":"image2.jpg", "type":"f", "size":10234,
+                                           "mime":"image/jpg", "mtime":"2014/03/01 14:18:21"}
+                                      ]},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        result = self.storage.list(path="/subfolder")
+        self.assert_(result["items"])
+        self.assertEqual(result["items"][0]["name"], "image2.jpg")
+
+        # list types
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {"items":[
+                                          {"name":"subfolder", "type":"d", "size":0,
+                                           "mime":"", "mtime":"2014/03/03 12:00:00"}
+                                      ]},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        result = self.storage.list(path="/",filetype="d")
+        self.assert_(len(result["items"])==1)
+        self.assertEqual(result["items"][0]["name"], "subfolder")
+
+
+    def test_list_failure(self):
+        # if result empty
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": {},
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        result = self.storage.list()
+        self.assertFalse(result["items"])
+
+        # if result none
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 200,
+                                      "content": None,
+                                      "headers": {"Content-Type":"application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+
+        result = self.storage.list()
+
+    def test_list_codes(self):
+        # code 400
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 400,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.InvalidParameter, self.storage.list)
+
+        # code 403
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 403,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.Forbidden, self.storage.list)
+
+        # code 404
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 404,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.ClientFailure, self.storage.list)
+
+        # code 500
+        r = adapter.StoredResponse(name="mystorage",
+                                   method="list",
+                                   httpmethod="POST",
+                                   response={
+                                      "status_code": 500,
+                                      "content": {},
+                                      "headers": {"Content-Type": "application/json"}
+                                   })
+        self.storage.session.responses=(r,)
+        self.assertRaises(endpoint.ServiceFailure, self.storage.list)
+
